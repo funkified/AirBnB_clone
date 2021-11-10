e9c75e09857d46330550b5aaf1c0e1b09b244bb3
KevinTMO <kevin.ramos3@hotmail.com>
2021-11-10 11:47:40 +0000

    Adding a new test file and changes to engine FileStorage class to load json file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Private class attributes:
-   - __file_path: string - path to the JSON file (ex: file.json)
-   -__objects: dictionary - empty but will store all objects by <class name>.id (ex: to store a BaseModel object with id=12121212, the key will be BaseModel.12121212)
+"""
+Module: file_storage.py
+
+Private class attributes:
+   - __file_path
+     - a string path to the JSON file (In this case "file.json")
+   -__objects
+     - An empty dictionary that will store all objects. For ClassName will be
+       saved like: Base.id -> format -> "{}.{}".format(self.className, self.id)
+
 Public instance methods:
-   - all(self): returns the dictionary __objects
-   - new(self, obj): sets in __objects the obj with key <obj class name>.id
-   - save(self): serializes __objects to the JSON file (path: __file_path)
-   - reload(self): deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+   - all(self)
+   - new(self, obj)
+   - save(self)
+   - reload(self)
 """
 
 from models.base_model import BaseModel
@@ -25,13 +32,23 @@ class FileStorage:
 
     def new(self, obj):
         """
+        Creating a new key for the base.id format
+        and adding to the dictionary
         """
         key = obj.__class__.__name__ + '.' + obj.id
+        # key = "{}.{}".format(self.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """save to json """
+        """
+        Serializing to a json string
+        We iterate through the dictionary items, give them to key,value
+        Calling "to_dict" method to create a dictionary with our format
+        then we give all the objects key/val to our new dict so we can save it
+        in as Json File
+        """
         new_dict = {}
+
         for key, value in self.__objects.items():
             new_dict[key] = self.__objects[key].to_dict()
 
@@ -39,4 +56,19 @@ class FileStorage:
             json.dump(new_dict, File)
 
     def reload(self):
-        pass
+        """
+        Deserialize from a json File
+        Using try/except to handle exception if need it,
+        We are going to try if the file exist to read it
+        Then convert everything as a dictionary object like before
+        if doesn't exist then pass since no exception need it right now
+        """
+        newDict = {}
+
+        try:
+            with open(self.__file_path, "r") as File:
+                newDict = json.loads(File)
+            return newDict
+
+        except:
+            pass
