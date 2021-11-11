329e9f139342c25074754f91e23f6a1db44a469b
KevinTMO <kevin.ramos3@hotmail.com>
2021-11-11 16:48:39 +0000

    Starting the console

--- console.py
+++ console.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
-
-import cmd, models
-
 """
+Module: console.py
+
+Import Modules:
+    - cmd
+    - models
+
+ClassDefinition:
+    - HBNBCommand(cmd.Cmd)
+      - cmd.Cmd: is a class inherited from module "cmd"
+
+Methods:
+    - do_quit(self)
+    - do_EOF(self, line)
+    - emptyline(self)
+
+Note:
+    - Help command is a built-in command so there's not need to add method
+      just prompt message as doc in every method
 """
+
+
+import cmd
+from models.base_model import BaseModel
+
+
 class HBNBCommand(cmd.Cmd):
+    """
+    Declare class instances:
+       - intro: welcome message output
+       - prompt: prompt message (ex: (hbnb))
+    """
+    intro = "Welcome to HBNB shell interpreter! Type ? to list commands"
     prompt = '(hbnb) '
-    """
-    def do_create(self, args):
-        pass
 
-    def do_show(self, args):
-        pass
-
-    def do_destroy(self):
-        pass
-
-    def do_all(self, args):
-        print(models.BaseModel())
-    """
     def do_quit(self, line):
+        'Quit command to exit the program'
         return True
 
     def do_EOF(self, line):
+        'Implement a clean way to exit: ctrl-D'
         print()
         return True
 
+    def emptyline(self):
+        'If the line is empty will not execute anything'
+        return False
 
-if __name__== '__main__':
+# Below is the cmd loop
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
